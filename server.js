f3a48cfac639f26717140de8a8420a35163af8df
Lia OBrien <liaobrien123@gmail.com>
2021-10-07 16:28:29 -0400

    filled out post request, add helper function to create id's for each note

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const noteData = require('./db/db.json');
 
@@ -22,13 +23,52 @@ app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-
 app.post('/api/notes', (req, res) => {
       console.info(`${req.method} request received to add a review`);
 
       // not sure where to go from here just yet...
-})
+      const { title, text } = req.body;
+
+      if (title && text) {
+            // Variable for the object we will save
+            const newNote = {
+                  title,
+                  text,
+                  review_id: uuid(),
+            };
+
+            fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+                  if (err) {
+                        console.error(err);
+                  } else {
+                        // Convert string into JSON object
+                        const parsedReviews = JSON.parse(data);
+
+                        // Add a new review
+                        parsedReviews.push(newNote);
+
+                        // Write updated reviews back to the file
+                        fs.writeFile(
+                              './db/reviews.json',
+                              JSON.stringify(parsedNotes, null, 4),
+                              (writeErr) =>
+                                    writeErr
+                                          ? console.error(writeErr)
+                                          : console.info('Successfully updated notes!')
+                        );
+                  }
+            });
+
+            const response = {
+                  status: 'success',
+                  body: newNote,
+            };
+
+            console.log(response);
+            res.status(201).json(response);
+      }
+});
 
 app.listen(PORT, () =>
-      console.log(`Example app listening at http://localhost:${PORT}`)
+      console.log(`App listening at http://localhost:${PORT} 🚀`)
 );

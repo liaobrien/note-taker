0ae01367856b8bc8a6a3b30739a555eb4583b6fa
Lia OBrien <liaobrien123@gmail.com>
2021-10-06 21:48:32 -0400

    created server.js and added baisic variables and get requests

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const noteData = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(__dirname));
+
+app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+app.listen(PORT, () =>
+      console.log(`Example app listening at http://localhost:${PORT}`)
+);

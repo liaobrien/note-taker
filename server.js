419bd132a8cb54f7c1f5fffb78a043fb887399cb
Lia OBrien <liaobrien123@gmail.com>
2021-10-07 17:03:03 -0400

    notes page is now displaying properly and adding notes to json

--- server.js
+++ server.js
@@ -3,26 +3,38 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
+const util = require('util');
 
 const noteData = require('./db/db.json');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
       res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-      res.json(noteData);
+      readFileAsync("./db/db.json", "utf8").then(data => {
+            const parsedData = JSON.parse(data);
+            res.json(parsedData);
+            res.end();
+      });
 })
 
 app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+
+// POST / api / notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post('/api/notes', (req, res) => {
       console.info(`${req.method} request received to add a review`);
 
@@ -34,22 +46,22 @@ app.post('/api/notes', (req, res) => {
             const newNote = {
                   title,
                   text,
-                  review_id: uuid(),
+                  note_id: uuid(),
             };
 
-            fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+            fs.readFile('./db/db.json', 'utf8', (err, data) => {
                   if (err) {
                         console.error(err);
                   } else {
                         // Convert string into JSON object
-                        const parsedReviews = JSON.parse(data);
+                        const parsedNotes = JSON.parse(data);
 
                         // Add a new review
-                        parsedReviews.push(newNote);
+                        parsedNotes.push(newNote);
 
                         // Write updated reviews back to the file
                         fs.writeFile(
-                              './db/reviews.json',
+                              './db/db.json',
                               JSON.stringify(parsedNotes, null, 4),
                               (writeErr) =>
                                     writeErr
